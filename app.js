37d7cb1028bfbfbeab1da1778033c2ed2a076d9a
akozlova <annakozlova92@gmail.com>
2021-06-09 19:05:31 -0700

    wip

--- app.js
+++ app.js
@@ -4,22 +4,17 @@
 // generate 3x3 grid of tiles with human in the center
 // each tile has an image, specie's name and a fact
 // for human tile => name and no fact
+const compareBtn = document.getElementById('btn');
 
 // Create Dino Constructor
-function Dino (speciesName, img, fact) {
+function Dino (species, weight, height, diet) {
     return {
-        speciesName,
-        img,
-        fact
+        species,
+        weight,
+        height,
+        diet
     }
 }
-
-const imgsArray = [];
-const fact = [];
-
-// Create Dino Objects
-const Triceraptos = Dino('Triceraptos', imgsArray[0], fact);
-const Brontozavr = Dino('Brontozavr', imgsArray[1], fact);
 
 // Create Human Object
 function Human (name, height, weight, diet) {
@@ -30,4 +25,25 @@ function Human (name, height, weight, diet) {
         diet
     }
 }
-const newHuman = Human('Vasya', 180, 160, 'herbavor');
+
+// compare Dino and human by weight
+const compareByWeight = (dinoName, dinoWeight, humanName, humanWeight) => {
+    const times = dinoWeight / humanWeight;
+    console.log(`${dinoName} is heavier ${times} times than ${humanName}`);
+}
+
+const compareByHeight = (dinoName, dinoHeight, humanName, humanHeight) => {
+    const times = dinoHeight / humanHeight;
+    console.log(`${dinoName} is heavier ${times} times than ${humanName}`);
+}
+
+const compareByDiet = (dinoName, dinoDiet, humanName, humanDiet) => {
+    console.log(`${dinoName} is ${dinoDiet} and ${humanName} is ${humanDiet}`);
+}
+
+compareBtn.addEventListener('click', function() {
+    compareByWeight('Dino', 100, 'human', 25);
+    compareByHeight('Dino', 120, 'human', 20);
+    compareByDiet('Dino', 'carnivor', 'human', 'herbavor');
+    
+});
